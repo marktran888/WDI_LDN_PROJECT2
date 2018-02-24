6ee02773170775c6d1c9b57c05a266415e2094b6
Mark Tran <mark.tran@hotmail.co.uk>
2018-02-24 15:38:45 +0000

    added dummy data for blog and respective route

--- config/router.js
+++ config/router.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const blog = require('../data/blog');
+
+router.get('/blog', (req, res) => res.render('pages/blog', { data: blog }));
 
 router.get('/', (req, res) => res.render('pages/home'));
 router.get('/about', (req, res) => res.render('pages/about'));
