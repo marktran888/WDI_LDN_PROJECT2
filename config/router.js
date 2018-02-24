0d547259dfa31f89a666a1faea3561c018f2bb73
Mark Tran <mark.tran@hotmail.co.uk>
2018-02-24 14:50:59 +0000

    added router and about and contact pages

---
+++ config/router.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => res.render('pages/home'));
+router.get('/about', (req, res) => res.render('pages/about'));
+router.get('/contact', (req, res) => res.render('pages/contact'));
+
+module.exports = router;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 const expressLayouts = require('express-ejs-layouts');
+const router = require('./config/router');
 
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
@@ -10,6 +11,8 @@ app.use(expressLayouts);
 app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => res.render('pages/home'));
+
+app.use(router); // MUST BE PLACED JUST BEFORE app.listen
 
 // app.listen should always be the last line in this file
 app.listen(PORT, () => console.log(`Up and running on port ${PORT}`));
