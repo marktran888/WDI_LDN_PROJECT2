2ff8a92e10987467d46f4b85b96a81cc6e9b10ad
Mark Tran <mark.tran@hotmail.co.uk>
2018-02-24 13:10:32 +0000

    added public folder. set up view engine and ejs layouts

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
+const expressLayouts = require('express-ejs-layouts');
 
+app.set('view engine', 'ejs');
+app.set('views', `${__dirname}/views`);
+app.use(expressLayouts);
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => res.render('pages/home'));
+
+// app.listen should always be the last line in this file
 app.listen(PORT, () => console.log(`Up and running on port ${PORT}`));
