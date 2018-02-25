c29e2b8749557e18f4a50f0e53591e50b660ac42
Mark Tran <mark.tran@hotmail.co.uk>
2018-02-25 18:04:16 +0000

    added home page and navbar

--- data/blog.js
+++ data/blog.js
@@ -6,7 +6,7 @@ module.exports = [{
   article: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 }, {
   title: 'Harry Potter',
-  director: 'Chris Columbus',
+  blogger: 'Chris Columbus',
   image: 'https://files.readanybook.com/786122/files/harry-potter.jpg',
   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do',
   article: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
