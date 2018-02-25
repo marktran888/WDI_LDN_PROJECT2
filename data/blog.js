8a3729c464e185c8779e5c66aee800230d03827c
Mark Tran <mark.tran@hotmail.co.uk>
2018-02-25 10:49:09 +0000

    added bulma styling

--- data/blog.js
+++ data/blog.js
@@ -2,10 +2,12 @@ module.exports = [{
   title: 'Lord of the Rings',
   blogger: 'Peter Jackson',
   image: 'https://upload.wikimedia.org/wikipedia/en/8/87/Ringstrilogyposter.jpg',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do',
   article: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 }, {
   title: 'Harry Potter',
   director: 'Chris Columbus',
   image: 'https://files.readanybook.com/786122/files/harry-potter.jpg',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do',
   article: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 }];
