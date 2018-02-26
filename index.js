55b390b0bca6ee3ead1b6a99d1e976bf944aa1ad
Mark Tran <mark.tran@hotmail.co.uk>
2018-02-26 12:07:45 +0000

    added method override for updates

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser'); // makes sense of form data i.e. allows use to use req.body
+const methodOverride = require('method-override');
 const router = require('./config/router');
 const mongoose = require('mongoose');
 
@@ -16,6 +17,16 @@ app.use(express.static(`${__dirname}/public`));
 
 //setup body bodyParser MUST BE BEFORE ROUTER
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// setup method-override
+app.use(methodOverride(function (req) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    const method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
 
 app.get('/', (req, res) => res.render('pages/home'));
 
